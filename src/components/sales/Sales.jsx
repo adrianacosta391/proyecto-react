afb91bc4d236b608b045fe25c06ef273e1ec1d5d
adrian391 <adrianpcnueva@gmail.com>
2022-07-28 01:04:16 -0300

    clear

--- src/components/sales/Sales.jsx
+++ src/components/sales/Sales.jsx
@@ -2,19 +2,22 @@ import React, { useState, useContext} from 'react'
 import { cartContext } from "../context/CartContext";
 import { db } from "../../firebase/firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore"
-import Swal from 'sweetalert2'
+import {Link} from "react-router-dom"
 
 const Sales = () => {
 const { products } = useContext(cartContext);
+const { clear } = useContext(cartContext);
 const total = products.reduce((acc, el) => acc + el.price * el.qty, 0)
 const [name, setName] = useState('');
 const [number, setNumber] = useState('');
 const [mail, setMail] = useState('');
-const [idSale, setIdSale] = useState('');
- 
+const [idSale, setIdSale] = useState([]);
 
 
-    const checkout = () => { 
+
+
+
+     const checkout = () => { 
         const ventasCollection = collection(db, 'ventas');
         addDoc(ventasCollection,{
             nombre: name,
@@ -30,27 +33,36 @@ const [idSale, setIdSale] = useState('');
         })
         
         
+        
     }
 
 
   return (
-      <div className="cartShowCheckout">
-        <p className="cartShowText">Rellena tus datos para finalizar la compra</p>
-        <p className="cartShowText">Nombre:</p>
-        <input className='inputChekout' onChange={e => setName(e.target.value)} type="text" />
-        <p className="cartShowText">Telefono:</p>
-        <input className='inputChekout'onChange={e => setNumber(e.target.value)} type="text" />
-        <p className="cartShowText">E-mail:</p>
-        <input className='inputChekout' onChange={e => setMail(e.target.value)} type="text" />
-        <button className='btn' onClick={checkout}>COMPRAR</button>
-        {idSale ? Swal.fire({
-            title: 'Tu compra se ha realizado con exito',
-            text: `El id de tu compra es: ${idSale}`,
-            icon: 'success',
-            confirmButtonText: 'ACEPTAR',
-            confirmButtonColor: '#3085d6'
-          }) : <p className="cartShowText">Hubo un error intentalo de nuevo</p>}
+<div>
+    {idSale.length === 0 
+    ? <div className="cartShowCheckout">
+    <p className="cartShowText">Rellena tus datos para finalizar la compra</p>
+    <p className="cartShowText">Nombre:</p>
+    <input className='inputChekout' onChange={e => setName(e.target.value)} type="text" />
+    <p className="cartShowText">Telefono:</p>
+    <input className='inputChekout'onChange={e => setNumber(e.target.value)} type="text" />
+    <p className="cartShowText">E-mail:</p>
+    <input className='inputChekout' onChange={e => setMail(e.target.value)} type="text" />
+    <button className='btn' onClick={checkout}>COMPRAR</button>
+</div>
+: 
+<div>
+    <div>
+        <p className="cartShowText"> Tu compra se ha realizado con exito</p>
+        <p className="itemDescDesc"> el ID de tu compra es: {idSale}</p>
+        <Link className="link" to="/"> <button className="btn" onClick={clear}>VOLVER</button></Link> 
     </div>
+
+</div>
+}
+</div>     
+        
+        
   )
 }
 
